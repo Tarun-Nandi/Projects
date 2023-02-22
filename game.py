91d5cd3ac8d9bd728fe689a4443718d3511cbf0e
ttn673 <66778709+ttn673@users.noreply.github.com>
2023-02-22 22:35:20 +0000

    first

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
+import pygame
+import sys
+
+class cargame():
+    def __init__(self):
+        pass
+
+    def game_loop():
+        while True: 
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit()
+
+            pygame.display.update()
+    
+
+    def car(self):
+        pass
 
