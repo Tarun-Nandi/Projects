780dcde3a7b7319969a6574fd8927e243a29215a
Tarun Tej Nandi <66778709+ttn673@users.noreply.github.com>
2023-02-22 23:18:51 +0000

    second

--- game.py
+++ game.py
@@ -2,13 +2,15 @@ import pygame
 import sys
 
 class cargame():
-    def __init__(self):
-        pass
+    def __init__(self,max_vel, rotation_vel):
+        self.max_vel= max_vel
+        self.rotation_vel = rotation_vel
+        self.vel = 0
 
-    def game_loop():
+    def game_loop(self):
         while True: 
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     sys.exit()
 
             pygame.display.update()
